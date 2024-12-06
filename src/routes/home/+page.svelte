b39971587d0143e0a2aa5fcf55a294cdf2480260
Nacola04 <92257036+Nacola04@users.noreply.github.com>
2024-12-06 01:08:38 +0100

    init home page

---
+++ src/routes/home/+page.svelte
@@ -0,0 +1,27 @@
+<div class="flex flex-col min-h-screen bg-surface-500">
+    <script src="https://kit.fontawesome.com/ff535060ef.js" crossorigin="anonymous"></script>
+    <header class="bg-primary-500 text-white py-4 shadow-md">
+        <div class="logo-cloud grid-cols-1 lg:!grid-cols-3 gap-1 mx-8 space-x-8">
+            <a class="logo-item" href="/">
+                <i class="fa-solid fa-circle-question"></i>
+                <span>Quizz</span>
+            </a>
+            <a class="logo-item" href="/">
+                <i class="fa-solid fa-ship"></i>
+                <span>Bataille navale</span>
+            </a>
+            <a class="logo-item" href="/">
+                <i class="fa-solid fa-address-card"></i>
+                <span>Credit</span>
+            </a>
+        </div>
+    </header>
+
+    <div class="flex flex-row">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe commodi laboriosam quisquam eaque, eius unde. Ducimus nobis quidem excepturi? Reiciendis, illum inventore. Impedit commodi est, eos aliquam sunt harum vero.</p>
+        <div class="flex flex-col w-36 h-36">
+            <img alt="" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRONglDk8Z5Fvbgeejieub9bZDsDMMjBWVEtQ&s"/>
+        </div>
+    </div>
+
+</div>
